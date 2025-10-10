794d062db1e2cda33a2a0422fb7fcff9bae257b7
lonely_cowboy <nikola.jokic994@yahoo.co.uk>
2025-10-10 03:46:52 +0200

    Fix responsive - add hamburger manu for small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,43 @@
-import "./Header.scss";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/" end>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/showreel">Showreel</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/blog">Blog</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-          
-        </ul>
+        <nav className="navbar">
+        
+        <div className="logo">MyLogo</div>
+
+        <div className={`nav-links ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <NavLink to="/" end onClick={() => setIsOpen(false)}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/showreel" onClick={() => setIsOpen(false)}>Showreel</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" onClick={() => setIsOpen(false)}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog" onClick={() => setIsOpen(false)}>Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" onClick={() => setIsOpen(false)}>Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </nav>
     </header>
   );
